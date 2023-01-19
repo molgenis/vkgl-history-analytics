06ed01f34ee538f17630ec676f860b3257d347b3
K. Joeri van der Velde <82420+joerivandervelde@users.noreply.github.com>
2023-01-19 21:42:16 +0100

    Main runs new analysis

--- src/main/java/org/molgenis/Main.java
+++ src/main/java/org/molgenis/Main.java
@@ -1,5 +1,7 @@
 package org.molgenis;
 
+import org.molgenis.consensuslevel.ConsensusLevelVKGLHistoryAnalytics;
+
 import java.io.File;
 
 public class Main {
@@ -7,9 +9,17 @@ public class Main {
     public static void main(String args[]) throws Exception {
         System.out.println("Starting...");
         long start = System.nanoTime();
-        VKGLHistoryAnalytics v = new VKGLHistoryAnalytics(new File(args[0]), new File(args[1]));
-        v.go();
+
+//        LabLevelVKGLHistoryAnalytics v = new LabLevelVKGLHistoryAnalytics(new File(args[0]), new File(args[1]));
+//        v.go();
+
+        ConsensusLevelVKGLHistoryAnalytics c = new ConsensusLevelVKGLHistoryAnalytics(new File(args[0]), new File(args[1]));
+        c.go();
+
         System.out.println("Done! Completed in " + ((System.nanoTime()-start)/1000000)+"ms.");
+
+
+
     }
 
 }
