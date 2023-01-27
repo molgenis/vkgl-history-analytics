bbf092a920d905c76856434f65b124e3a878fe46
K. Joeri van der Velde <82420+joerivandervelde@users.noreply.github.com>
2023-01-27 17:36:30 +0100

    Add SAID gene panel

---
+++ src/main/java/org/molgenis/genepanels/SAID.java
@@ -0,0 +1,66 @@
+package org.molgenis.genepanels;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SAID {
+
+    public static final List<String> genes = new ArrayList<>(Arrays.asList(
+            "MEFV",
+            "TREX1",
+            "NLRP1",
+            "NLRC4",
+            "COPA",
+            "OTULIN",
+            "TNFAIP3",
+            "NOD2",
+            "SH3BP2",
+            "NLRP3",
+            "CECR1",
+            "IL1RN",
+            "IL36RN",
+            "MVK",
+            "SLC29A3",
+            "LPIN2",
+            "ADAM17",
+            "PSTPIP1",
+            "POLA1",
+            "PLCG2",
+            "PSMB8",
+            "AP1S3",
+            "CARD14",
+            "TMEM173",
+            "ACP5",
+            "TNFRSF1A",
+            "LSM11",
+            "RNU7-1",
+            "RIPK1",
+            "SOCS1",
+            "NCKAP1L", // original: NCKAP1L/HEM-1
+            "TET2",
+            "STAT2",
+            "CEBPE",
+            "CDC42",
+            "UBA1",
+            "RSEH2B",
+            "RSEH2C",
+            "RSEH2A",
+            "SAMHD1",
+            "ADAR1",
+            "IFIH1", //original: IFIH1_GOF
+            "NLRP12",
+            "ALP1",
+            "ELF4",
+            "TRIM22", //original: trim22
+            "DSE2",
+            "OAS1",
+            "PSMG2", // original: psmg2
+            "PSMB10",
+            "USP18",
+            "ALPK1",
+            "SAMD9L",
+            "DSE1L3",
+            "HAVCR2"
+    ));
+}
