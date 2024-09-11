cb15c8761334b022bfc459416f4039cac9464de8
K. Joeri van der Velde <82420+joerivandervelde@users.noreply.github.com>
2024-09-11 13:05:42 +0200

    Update paper stats

--- src/main/R/alluvial-consensus.R
+++ src/main/R/alluvial-consensus.R
@@ -9,7 +9,7 @@ setwd("/Users/joeri/VKGL/VKGL-releases/july2024")
 curRel <- "july2024"
 curRelFull <- "July 2024"
 
-vch <- read.table("dataframe-said.tsv",header=TRUE,sep='\t',quote="",comment.char="")
+vch <- read.table("dataframe.tsv",header=TRUE,sep='\t',quote="",comment.char="")
 vch$Release <- factor(vch$Release, levels = c("may2018", "oct2018", "june2019", "oct2019", "dec2019", "mar2020", "jun2020", "sep2020", "apr2021", "jun2021", "sep2021", "dec2021", "sep2022", "jan2023", "apr2023", "july2023", "oct2023", "feb2024", "apr2024", "july2024"))
 vch$Release <- revalue(vch$Release, c("may2018"="May 2018", "oct2018"="Oct 2018", "june2019"="June 2019", "oct2019"="Oct 2019", "dec2019"="Dec 2019", "mar2020"="Mar 2020", "jun2020"="June 2020", "sep2020"="Sept 2020", "apr2021"="Apr 2021", "jun2021"="June 2021", "sep2021"="Sept 2021", "dec2021"="Dec 2021", "sep2022"="Sept 2022", "jan2023"="Jan 2023", "apr2023"="Apr 2023", "july2023"="July 2023", "oct2023"="Oct 2023", "feb2024"="Feb 2024", "apr2024"="Apr 2024", "july2024"="July 2024"))
 vch$Consensus <- factor(vch$Consensus)
@@ -92,7 +92,7 @@ total
 table(vch$Consensus)
 SLCO1B1_occ <- dim(subset(vch, Gene == "SLCO1B1"))[1]
 # on 'greater than 1 classification'
-dim(subset(vch, Release == "Apr 2024"))
+dim(subset(vch, Release == "July 2024"))
 oct18lblp <- subset(vch, Release == "Oct 2018" & (Consensus == "LB/B" | Consensus == "LP/P"))
 dim(oct18lblp)
 jun19vus <- subset(vch, Release == "June 2019" & Consensus == "VUS")
@@ -104,7 +104,7 @@ jun19vusToOct19lblp <- intersect(jun19vus$Id,oct19lblp$Id)
 length(jun19vusToOct19lblp)
 length(intersect(oct18lblpToJun19vus,jun19vusToOct19lblp))
 # on lp-lb or lb-lp transitions
-latest_data <- subset(vch, Release == "Apr 2024")
+latest_data <- subset(vch, Release == "July 2024")
 latest_total <- dim(latest_data)[1]
 latest_total
 latest_SLCO1B1_occ <- dim(subset(latest_data, Gene == "SLCO1B1"))[1]
