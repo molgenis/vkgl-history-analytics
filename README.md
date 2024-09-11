31d2fe51b28ddcc92353efe7346de8a5ddcd1c77
K. Joeri van der Velde <82420+joerivandervelde@users.noreply.github.com>
2024-09-11 08:57:52 +0200

    Update for July 2024

--- README.md
+++ README.md
@@ -3,13 +3,13 @@ A tool that parses VKGL variant classification data from public releases, connec
 Please cite [Fokkema et al. 2019](https://doi.org/10.1002/humu.23896).
 
 ## Overview visualizations
-![Latest release](img/vkgl-apr2024.png)
-![More than 1 class](img/vkgl-apr2024-gt1clsf.png)
-![LP-LB or LB-LP](img/vkgl-apr2024-lp-lb-trans.png)
-![Not in latest](img/vkgl-notinapr2024.png)
-![Multiclass](img/vkgl-apr2024-multiclass.png)
+![Latest release](img/vkgl-july2024.png)
+![More than 1 class](img/vkgl-july2024-gt1clsf.png)
+![LP-LB or LB-LP](img/vkgl-july2024-lp-lb-trans.png)
+![Not in latest](img/vkgl-notinjuly2024.png)
+![Multiclass](img/vkgl-july2024-multiclass.png)
 
 ## Examples of visualizations for specific chromosomes, genes or variant consequences
-![Y chromosome](img/vkgl-apr2024-y.png)
-![SAID indels](img/vkgl-apr2024-said-indels.png)
-![BRCA1 insertions](img/vkgl-apr2024-brca1-ins.png)
+![Y chromosome](img/vkgl-july2024-y.png)
+![SAID indels](img/vkgl-july2024-said-indels.png)
+![BRCA1 insertions](img/vkgl-july2024-brca1-ins.png)
--- src/main/R/alluvial-consensus.R
+++ src/main/R/alluvial-consensus.R
@@ -5,13 +5,13 @@ require(ggalluvial)
 require(RColorBrewer)
 require(plyr)
 
-setwd("/Users/joeri/VKGL/VKGL-releases/apr2024")
-curRel <- "apr2024"
-curRelFull <- "April 2024"
+setwd("/Users/joeri/VKGL/VKGL-releases/july2024")
+curRel <- "july2024"
+curRelFull <- "July 2024"
 
-vch <- read.table("dataframe.tsv",header=TRUE,sep='\t',quote="",comment.char="")
-vch$Release <- factor(vch$Release, levels = c("may2018", "oct2018", "june2019", "oct2019", "dec2019", "mar2020", "jun2020", "sep2020", "apr2021", "jun2021", "sep2021", "dec2021", "sep2022", "jan2023", "apr2023", "july2023", "oct2023", "feb2024", "apr2024"))
-vch$Release <- revalue(vch$Release, c("may2018"="May 2018", "oct2018"="Oct 2018", "june2019"="June 2019", "oct2019"="Oct 2019", "dec2019"="Dec 2019", "mar2020"="Mar 2020", "jun2020"="June 2020", "sep2020"="Sept 2020", "apr2021"="Apr 2021", "jun2021"="June 2021", "sep2021"="Sept 2021", "dec2021"="Dec 2021", "sep2022"="Sept 2022", "jan2023"="Jan 2023", "apr2023"="Apr 2023", "july2023"="July 2023", "oct2023"="Oct 2023", "feb2024"="Feb 2024", "apr2024"="Apr 2024"))
+vch <- read.table("dataframe-said.tsv",header=TRUE,sep='\t',quote="",comment.char="")
+vch$Release <- factor(vch$Release, levels = c("may2018", "oct2018", "june2019", "oct2019", "dec2019", "mar2020", "jun2020", "sep2020", "apr2021", "jun2021", "sep2021", "dec2021", "sep2022", "jan2023", "apr2023", "july2023", "oct2023", "feb2024", "apr2024", "july2024"))
+vch$Release <- revalue(vch$Release, c("may2018"="May 2018", "oct2018"="Oct 2018", "june2019"="June 2019", "oct2019"="Oct 2019", "dec2019"="Dec 2019", "mar2020"="Mar 2020", "jun2020"="June 2020", "sep2020"="Sept 2020", "apr2021"="Apr 2021", "jun2021"="June 2021", "sep2021"="Sept 2021", "dec2021"="Dec 2021", "sep2022"="Sept 2022", "jan2023"="Jan 2023", "apr2023"="Apr 2023", "july2023"="July 2023", "oct2023"="Oct 2023", "feb2024"="Feb 2024", "apr2024"="Apr 2024", "july2024"="July 2024"))
 vch$Consensus <- factor(vch$Consensus)
 vch$Consensus <- revalue(vch$Consensus, c("VUS"="VUS", "LB"="LB/B", "LP"="LP/P", "CF"="Multiple classifications", "Absent"="Absent from release"))
 
